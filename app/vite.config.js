5d694337395385384995615636052cf811fec294
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-02-23 12:47:04 -0300

    sitess

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'assets',
     rollupOptions: {
       external: [
-        'src/assets/img/Perfil.png',
-        'src/assets/icons/github.svg',
-        'src/assets/icons/insta.svg',
-        'src/assets/icons/linkdin.svg',
-        'src/assets/icons/whatsapp.svg',
-        'src/assets/icons/gmail.svg',
-        'src/assets/gifs/curriculo.gif'
+        // Add the file to externalize it
+        '/src/assets/img/Perfil.png',
       ],
-    }
-}})
+    },
+  },
+});
