e55dea49132be55781de30bb25bf71492ae71bc1
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-02-23 12:49:04 -0300

    vite.config

--- app/vite.config.js
+++ app/vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
       external: [
         // Add the file to externalize it
         '/src/assets/img/Perfil.png',
+        '/src/assets/icons/github.svg',
+        '/src/assets/icons/insta.svg',
+        '/src/assets/icons/linkdin.svg',
+        '/src/assets/icons/whatsapp.svg',
+        '/src/assets/icons/gmail.svg',
+        '/src/assets/gifs/curriculo.gif',
       ],
     },
   },
