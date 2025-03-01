ea6e8ad8a0a8fe52e38aba5d83c7b11542102e40
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-03-01 20:07:55 -0300

    add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -31,6 +31,7 @@ export default function App()
     React.useEffect(() => {
         ReactGA.initialize(GA_TRACKING_ID_TAG_DEFAULT);
         ReactGA.send({ hitType: "pageview", page: "/" });
+        alert('enviado')
       }, []);
 
 
