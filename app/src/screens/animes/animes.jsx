4c33d7a484c77e9c88e75d36ae76ee1ad82d94d9
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-04-21 16:52:28 -0300

    ajuste dos erros

--- app/src/screens/animes/animes.jsx
+++ app/src/screens/animes/animes.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {marked} from 'marked'
-import '@styles/animes/Animes.css'
+import '@styles/Animes/Animes.css'
 
 
 export default function Animes() {
