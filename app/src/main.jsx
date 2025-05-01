c53afc19b72b8e3e9bf8019ed1250cb0d035a1c5
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-05-01 14:54:33 -0300

    r

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import App from "./App";
 import Certificados  from './screens/certificados/certificados.jsx';
 import Animes        from "./screens/animes/animes.jsx";
